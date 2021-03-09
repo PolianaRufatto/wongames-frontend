831ee3883d9d9ec1f1bd597e31c781f706386934
Poliana Rufatto <polianarufatto@gmail.com>
2021-03-09 07:06:19 -0300

    Add tests/story ribbon to banner

--- src/components/Banner/test.tsx
+++ src/components/Banner/test.tsx
@@ -15,10 +15,36 @@ describe('<Banner />', () => {
   it('should render correctaly', () => {
     const { container } = renderWithTheme(<Banner {...props} />)
 
-    expect(screen.getByRole('heading', { name: /Defy death/i })).toBeInTheDocument()
-    expect(screen.getByRole('heading', { name: /Play the new CrashLands season/i })).toBeInTheDocument()
+    expect(
+      screen.getByRole('heading', { name: /Defy death/i })
+    ).toBeInTheDocument()
+    expect(
+      screen.getByRole('heading', { name: /Play the new CrashLands season/i })
+    ).toBeInTheDocument()
     expect(screen.getByRole('img', { name: /Defy death/i })).toBeInTheDocument()
 
     expect(container).toMatchSnapshot()
+  })
+
+  it('should render a Ribbon', () => {
+    renderWithTheme(
+      <Banner
+        {...props}
+        ribbon="My Ribbon"
+        ribbonSize="small"
+        ribbonColor="Secondary"
+      />
+    )
+
+    const ribon = screen.getByText(/My Ribbon/i)
+
+    expect(ribon).toBeInTheDocument()
+    expect(ribon).toHaveStyle({
+      backgroundColor: '#3CD3C1'
+    })
+    expect(ribon).toHaveStyle({
+      height: '2.6rem',
+      fontSize: '1.2 rem'
+    })
   })
 })
