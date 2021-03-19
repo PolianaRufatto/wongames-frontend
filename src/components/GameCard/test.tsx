8ded159aaa22fc0e5b2f025edf865f887160debd
Poliana Rufatto <polianarufatto@gmail.com>
2021-03-19 07:01:51 -0300

    Add GameCard initial tests

--- src/components/GameCard/test.tsx
+++ src/components/GameCard/test.tsx
@@ -1,13 +1,32 @@
-import { render, screen } from '@testing-library/react'
+import { screen } from '@testing-library/react'
+import { renderWithTheme } from 'utils/tests/helpers'
 
 import GameCard from '.'
 
+const props = {
+  title: 'Heading 1',
+  developer: 'Developer',
+  img: '/game-card-img.jpg',
+  price: 'R$ 230,00'
+}
+
 describe('<GameCard />', () => {
-  it('should render the heading', () => {
-    const { container } = render(<GameCard />)
+  it('should render correctly', () => {
+    renderWithTheme(<GameCard {...props} />)
 
-    expect(screen.getByRole('heading', { name: /GameCard/i })).toBeInTheDocument()
+    expect(
+      screen.getByRole('heading', { name: props.title })
+    ).toBeInTheDocument()
 
-    expect(container.firstChild).toMatchSnapshot()
+    expect(
+      screen.getByRole('heading', { name: props.developer })
+    ).toBeInTheDocument()
+
+    expect(screen.getByRole('img', { name: props.title })).toHaveAttribute(
+      'src',
+      props.img
+    )
+
+    expect(screen.getByLabelText(/add to wishlist/i)).toBeInTheDocument()
   })
 })
