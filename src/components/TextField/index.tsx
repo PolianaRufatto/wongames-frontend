e718c5307ab6cbf5f39c299145afe7baa93af2a8
Poliana Rufatto <polianarufatto@gmail.com>
2021-05-05 05:15:56 -0300

    Add option to icon on TextField

---
+++ src/components/TextField/index.tsx
@@ -0,0 +1,40 @@
+import { InputHTMLAttributes, useState } from 'react'
+import * as S from './styles'
+
+export type TextFieldProps = {
+  onInput?: (value: string) => void
+  icon?: React.ReactNode
+  label?: string
+  labelFor?: string
+  initialValue?: string
+} & InputHTMLAttributes<HTMLInputElement>
+
+const TextField = ({
+  onInput,
+  icon,
+  label,
+  labelFor = '',
+  initialValue = '',
+  ...props
+}: TextFieldProps) => {
+  const [value, setValue] = useState(initialValue)
+
+  const onChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const newValue = e.currentTarget.value
+    setValue(newValue)
+
+    !!onInput && onInput(newValue)
+  }
+
+  return (
+    <S.Wrapper>
+      {!!label && <S.Label htmlFor={labelFor}>{label}</S.Label>}
+      <S.InputWrapper>
+        {!!icon && <S.Icon>{icon}</S.Icon>}
+        <S.Input type="text" onChange={onChange} value={value} {...props} />
+      </S.InputWrapper>
+    </S.Wrapper>
+  )
+}
+
+export default TextField
