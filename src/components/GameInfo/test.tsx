061c7029daa69491a9a7640fb844d19ef5555126
Poliana Rufatto <polianarufatto@gmail.com>
2021-07-28 07:59:22 -0300

    Add html and tests to GameInfo

--- src/components/GameInfo/test.tsx
+++ src/components/GameInfo/test.tsx
@@ -1,3 +1,4 @@
+import { screen } from '@testing-library/react'
 import { renderWithTheme } from 'utils/tests/helpers'
 
 import GameInfo from '.'
@@ -5,15 +6,28 @@ import GameInfo from '.'
 const props = {
   title: 'Game title',
   description: 'Game description',
-  price: '140.00'
+  price: '140,00'
 }
 
 describe('<GameInfo />', () => {
   it('should render game informations', () => {
     renderWithTheme(<GameInfo {...props} />)
+
+    expect(
+      screen.getByRole('heading', { name: /game title/i })
+    ).toBeInTheDocument()
+    expect(screen.getByText(/\$140,00/)).toBeInTheDocument()
+    expect(screen.getByText(/game description/i)).toBeInTheDocument()
   })
 
   it('should render buttons', () => {
     renderWithTheme(<GameInfo {...props} />)
+
+    expect(
+      screen.getByRole('button', { name: /add to cart/i })
+    ).toBeInTheDocument()
+    expect(
+      screen.getByRole('button', { name: /wishlist/i })
+    ).toBeInTheDocument()
   })
 })
